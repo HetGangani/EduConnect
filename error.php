fca26520880fd66603356da5dbe0afde11c6f67d
Het Gangani <135121816+HetGangani@users.noreply.github.com>
2025-10-08 00:35:39 +0530

    Update error.php

--- error.php
+++ error.php
@@ -2,12 +2,13 @@
 <html>
 <head>
   <title>Registration Failed</title>
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="message error">
     <h2>❌ Registration Failed!</h2>
-    <p>There was a problem saving your data. Please <a href="../html/register.html">try again</a>.</p>
+    <p>There was a problem saving your data. Please <a href="register.html">try again</a>.</p>
   </div>
 </body>
+
 </html>
