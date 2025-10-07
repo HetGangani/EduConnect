b41dcdc17a20c9683727113590ed340477e8cbae
Het Gangani <135121816+HetGangani@users.noreply.github.com>
2025-10-08 00:33:23 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION["username"])) {
-    header("Location: ../html/login.html");
+    header("Location: login.html");
     exit;
 }
 ?>
@@ -12,17 +12,18 @@ if (!isset($_SESSION["username"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | EduConnect</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php if($_SESSION['role'] === 'admin'): ?>
-    <a href="../php/manage_events.php">Manage Events</a>
+    <a href="manage_events.php">Manage Events</a>
     <?php endif; ?>
 
     <h1>Welcome, <?php echo htmlspecialchars($_SESSION["username"]); ?> 👋</h1>
     <p>Your Role: <?php echo htmlspecialchars($_SESSION["role"]); ?></p>
     <p>You are logged in successfully!</p>
 
-    <a href="../php/logout.php">Logout</a>
+    <a href="logout.php">Logout</a>
 </body>
 </html>
+
